b9155876254a384affa5b049076451f113723c5f
iplmr <45397969+iplmr@users.noreply.github.com>
2024-04-10 16:30:32 +0300

    adding input number 1 and input number 2 and input for operation , finaly i make switch statemaent to mang out put

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+Console.WriteLine("Welcome to Calculater C sharp\n");
+
+Console.WriteLine("Enter first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter seconed number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter The Opertion Like (* , / , + , - , %)");
+string? opertion = Console.ReadLine();
+
+var opertionSwitch = opertion switch{
+    "+" => $"{num1} + {num2} =  {num1 + num2}",
+    "-" => $"{num1} - {num2} = {num1 - num2} ",
+    "*" => $"{num1} * {num2} = {num1 * num2}",
+    "/" => $"{num1} / {num2} = {num1 / num2}",
+    "%" => $"{num1} % {num2} = {num1 % num2}",
+    _ => "please Enter Operation like (* , / , + , - , %))"
+};
+Console.WriteLine(opertionSwitch);
+
